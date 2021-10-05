23eb7c6b26fabb1537e74c22cc91c38b0de20373
SamKuo0413 <89826538+SamKuo0413@users.noreply.github.com>
2021-10-05 20:49:51 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,12 @@ radio.onReceivedNumber(function (receivedNumber) {
         PingUnit.Centimeters
         )
         if (第一台距離一 > 第一台距離二) {
-            radio.sendNumber(0)
+            radio.sendNumber(1)
+            第一台距離一 = sonar.ping(
+            DigitalPin.P0,
+            DigitalPin.P1,
+            PingUnit.Centimeters
+            )
         }
     }
     if (receivedNumber == 2) {
@@ -16,7 +21,12 @@ radio.onReceivedNumber(function (receivedNumber) {
         PingUnit.Centimeters
         )
         if (第二台距離一 > 第二台距離二) {
-            radio.sendNumber(0)
+            radio.sendNumber(2)
+            第二台距離一 = sonar.ping(
+            DigitalPin.P2,
+            DigitalPin.P3,
+            PingUnit.Centimeters
+            )
         }
     }
     if (receivedNumber == 3) {
@@ -26,7 +36,12 @@ radio.onReceivedNumber(function (receivedNumber) {
         PingUnit.Centimeters
         )
         if (第三台距離一 > 第三台距離二) {
-            radio.sendNumber(0)
+            radio.sendNumber(3)
+            第三台距離一 = sonar.ping(
+            DigitalPin.P5,
+            DigitalPin.P4,
+            PingUnit.Centimeters
+            )
         }
     }
 })
